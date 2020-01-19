c1d80f1f9572d8901b556c4457053da4a466f0e7
carmc99 <carmc99@gmail.com>
2020-01-19 10:29:01 -0500

    Agregar archivos de proyecto.

---
+++ PokedesRPA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using PokedesRPA.com.facade;
+
+
+// Aplicacion para la automatizacion de la busqueda de pokemon's
+namespace PokedesRPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Dictionary<string, object> pokemonData = new Dictionary<string, object>();
+            pokemonData.Add("id", 1);
+            pokemonData.Add("name", "Pikachu");
+            pokemonData.Add("type1", "");
+            pokemonData.Add("type2", "");
+            pokemonData.Add("group1", "");
+            pokemonData.Add("group2", "");
+            pokemonData.Add("imagePath", "");
+            Pokedex pk = new Pokedex();
+            pk.register(pokemonData);
+
+            pk.getAll();
+           
+
+        }
+    }
+}
---
+++ PokedesRPA/com.models/Pokemon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokedesRPA.com.models
+{
+    class Pokemon
+    {
+        private int id { set; get; }
+        private string name { set; get; }
+        private string type1 { set; get; }
+        private string type2 { set; get; }
+        private string nameJapane { set; get; }
+        private string group1 { set; get; }
+        private string group2 { set; get; }
+        private string imagePath { set; get; }
+
+        public Pokemon(Dictionary<string, object> pokemonData)
+        {
+            this.id = (int) pokemonData["id"];
+            this.name = (string) pokemonData["name"];
+            this.type1 = (string) pokemonData["type1"];
+            this.type2 = (string) pokemonData["type2"];
+            this.group1 = (string) pokemonData["group1"];
+            this.group2 = (string) pokemonData["group2"];
+            this.imagePath = (string) pokemonData["imagePath"];
+        }
+    }
+}
